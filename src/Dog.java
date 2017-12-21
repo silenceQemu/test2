c8eb4a55a1e70223964f4ca60d6a9981f1e16a0c
silenceQemu <18800205508@163.com>
2017-12-21 15:27:21 +0800

    hi~

---
+++ src/Dog.java
@@ -0,0 +1,15 @@
+public class Dog {
+    String name;
+    public void bark(){
+        System.out.println(name + "say Ruff!");
+    }
+    public void eat(){
+
+    }
+    public void chaseCat(){
+
+    }
+
+
+
+}
